50c6d6e9b669ebee1af3959be5e799be91cc2fe8
Gabriel Lima <dev.gabriel.lima@gmail.com>
2013-11-19 22:13:02 -0200

    Adicionado modulo graficos

---
+++ BlockBuster/Controllers/ChartController.cs
@@ -0,0 +1,36 @@
+﻿using BlockBuster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace BlockBuster.Controllers
+{
+    public class ChartController : Controller
+    {
+        private Repository db = new Repository();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult AtoresEOTotalDeTitulosPorAtor()
+        {
+            List<Object[]> data = new List<Object[]>();
+            var atores = db.Atores.ToList();
+
+            foreach (var ator in atores)
+            {
+                data.Add(new Object[] { ator.Nome, ator.Titulos.Count() });
+            }
+
+            return Json(data.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
+
+
+
